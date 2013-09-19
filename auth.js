086307532be7bedc4640a52764b389945e30b51a
OpenShift guest <sarevook@gmail.com>
2013-09-19 17:08:34 -0400

    added auth example

---
+++ auth.js
@@ -0,0 +1,33 @@
+var passport = require("passport"),
+    LocalStrategy = require('passport-local').Strategy;
+    
+app.use(express.cookieParser());
+app.use(express.bodyParser());
+app.use(express.session({ secret: 'SECRET' }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+var LocalUserSchema = new mongoose.Schema({
+username: String,
+salt: String,
+hash: String
+});
+
+var Users = mongoose.model('userauths', localUserSchema);
+
+
+passport.use(new LocalStrategy(function(username, password,done){
+    Users.findOne({ username : username},function(err,user){
+        if(err) { return done(err); }
+        if(!user){
+            return done(null, false, { message: 'Incorrect username.' });
+        }
+
+        hash( password, user.salt, function (err, hash) {
+            if (err) { return done(err); }
+            if (hash == user.hash) return done(null, user);
+            done(null, false, { message: 'Incorrect password.' });
+        });
+    });
+}));
